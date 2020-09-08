53d64dd62e69fb4c5f8609586c586dbd2f1812ec
julgoetz <70372980+jugoetz@users.noreply.github.com>
2020-09-08 15:33:30 +0200

    Remove celery_beat from apps

--- ChemicalBorrowingWebApp/settings.py
+++ ChemicalBorrowingWebApp/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'inventorymanagement.apps.InventorymanagementConfig',
-    #'django_celery_beat',
 ]
 
 MIDDLEWARE = [
