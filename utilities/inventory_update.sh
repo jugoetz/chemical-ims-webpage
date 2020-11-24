1a548a1c538b4e8b9e3f8bb94a32cac08d7159f4
jugoetz <70372980+jugoetz@users.noreply.github.com>
2020-11-24 10:39:33 +0100

    bugfix in inventory_update.sh

--- utilities/inventory_update.sh
+++ utilities/inventory_update.sh
@@ -4,11 +4,15 @@
 # It assumes that the venv django runs in is located in the same directory as the django source directory
 # Run the database update for sqlite3 database of chemical-ims-webpage by calling updateFromExpereact.py
 
+# get script directory and make it pwd
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd $DIR || exit 1
+
 # activate venv
 source ../../.venv/bin/activate
 
 # execute python script
-cd ../chemical-ims-webpage/inventorymanagement/ || exit 1
+cd ../inventorymanagement/ || exit 1
 python updateFromExpereact.py
 
 # deactivate venv
